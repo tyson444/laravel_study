84119bafddb19388bf1c2cf4b15de5cd87924391
tyson444 <tyson444@yahoo.com>
2017-01-28 18:49:09 +0900

    입력값 유효성 검사

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests\ArticlesRequest; 
+
 class ArticlesController extends Controller
 {
 
@@ -14,8 +16,60 @@ class ArticlesController extends Controller
         //$articles->load('user');
         //$articles = \App\Article::with('user')->get();
         
-        $articles = \App\Article::latest()->paginate(3);
+        //$articles = \App\Article::latest()->paginate(3);
+        $articles = \App\Article::oldest()->paginate(3);
         return view('articles.index', compact('articles'));
+    }
+    public function create()
+    {
+        return view('articles.create');
+        //return __METHOD__ . '2은(는) Article 컬렉션을 만들기 위한 폼을 담은 뷰를 반환합니다.';
+    }
+/*
+    public function store(\App\Http\Requests\ArticlesRequest $request)
+    {
+*/
+    
+    public function store(Request $request)
+    {
+
+/*
+        $rules = [
+            'title' => ['required'],
+            'content' => ['required', 'min:10'],
+        ];
+
+        //$validator = \Validator::make($request->all(),$rules);
+
+        $messages = [
+                    'title.required' => '제목은 필수 입력 항목입니다.',
+                    'content.required' => '본문은 필수 입력 항목입니다.',
+                    'content.min' => '본문은 최소 :min 글자 이상이 필요합니다.',
+        ];
+
+        $validator = \Validator::make($request->all(),$rules, $messages);
+
+        //$this->validate($request, $rules, $messages);
+
+        $article = \App\User::find(1)->articles()->create($request->all());
+
+
+        if($validator->fails()){
+            return back()->withErrors($validator)
+                        ->withInput();
+        }
+*/
+
+        $article = \App\User::find(1)->articles()
+                                    ->create($request->all());
+
+        if (! $article) {
+            return back()->with('flash_message','글이 저장되지 않았습니다.')
+                        ->withInput();
+        }
+
+        return redirect(route('articles.index'))
+                ->with('flash_message', '작성하신 글이 저장되었습니다.');
     }
 
 /*
